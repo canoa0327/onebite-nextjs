d0fa28a764e87987a46bab5b7b921228b5ad52ef
canoa <canoa950327@gmail.com>
2025-10-25 04:28:26 +0900

    Add section02 and section03 projects

---
+++ section02/src/pages/index.tsx
@@ -0,0 +1,66 @@
+import SearchableLayout from "@/components/searchable-layout";
+import style from "./index.module.css";
+import { ReactNode } from "react";
+import BookItem from "@/components/book-item";
+import { InferGetStaticPropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
+
+// export const getServerSideProps = async () => { ** SSR 방식
+export const getStaticProps = async () => {
+  // 컴포넌트보다 먼저 실행됨, 컴포넌트에 필요한 데이터 불러옴
+
+  // const allBooks = await fetchBooks();
+  // const recoBooks = await fetchRandomBooks(); 직렬정인 API 호출 방식
+
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: // }: InferGetServerSidePropsType<typeof getServerSideProps>) { ** SSR 방식
+InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입북스에 등록된 도서들을 만나보세요."
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
+  );
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
