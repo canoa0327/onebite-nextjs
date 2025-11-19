3e30e22d081e905406024d81f35173ad9d2ff006
canoa <canoa950327@gmail.com>
2025-11-19 17:39:16 +0900

    34. 3.4

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -4,5 +4,6 @@ export default async function Page({
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
+  console.log("ㅋㅋ");
   return <div>Search Page {q}</div>;
 }
